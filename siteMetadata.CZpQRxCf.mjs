5be1f0d442de49264e9be99492998ab564bd6755
louisdang1403 <71143210+louisdang1403@users.noreply.github.com>
2025-12-02 01:24:32 +0700

    Add files via upload

---
+++ siteMetadata.CZpQRxCf.mjs
@@ -0,0 +1,79 @@
+import{__esmMin as e}from"./rolldown-runtime.I8hAdsGi.mjs";function t(e,t){return{customHTMLHeadEnd:`<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Belleva Nail",
+  "url": "https://www.bellevanail.com/",
+  "image": "https://www.bellevanail.com/images/logo.jpg", 
+  "description": "Belleva Nail is a unique nail salon in Denton offering premium nail care services with exclusive CBD-infused treatments, eco-friendly products, and loyalty rewards.",
+  "telephone": "(940) 514-1808",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "2200 W. University Dr, Suite 180",
+    "addressLocality": "Denton",
+    "addressRegion": "TX",
+    "postalCode": "76201",
+    "addressCountry": "US"
+  },
+  "openingHours": [
+    "Mo-Fr 09:30-19:30",
+    "Sa 10:00-18:00",
+    "Su 11:00-17:00"
+  ],
+  "sameAs": [
+    "https://www.facebook.com/people/Belleva-Nails/61569011714593/",
+    "https://www.instagram.com/bellevanailsdenton/"
+  ],
+  "priceRange": "$$",
+  "serviceType": [
+    "Nail Care",
+    "Eyelash Services",
+    "CBD-infused Pedicure"
+  ],
+  "logo": "https://framerusercontent.com/images/64IlVmpDRsW80lI23LFvKpY.png",
+  "brand": "Belleva Nail",
+  "slogan": "We can't change the world, but we can change your nail style!",
+  "additionalType": "http://www.productontology.org/id/Beauty_care",
+  "offers": {
+    "@type": "Offer",
+    "url": "https://www.bellevanail.com/booking",
+    "priceCurrency": "USD",
+    "price": "Varies",
+    "priceValidUntil": "2025-12-31",
+    "eligibleRegion": {
+      "@type": "Place",
+      "name": "Denton"
+    }
+  },
+  "specialOpeningHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Monday",
+      "opens": "09:30",
+      "closes": "19:30"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Saturday",
+      "opens": "10:00",
+      "closes": "18:00"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Sunday",
+      "opens": "11:00",
+      "closes": "17:00"
+    }
+  ],
+  "mainEntityOfPage": "https://www.bellevanail.com/"
+}
+<\/script>
+`,customHTMLHeadStart:`<meta name="p:domain_verify" content="2e4094b3f5a8a9a199659f4ab1fe6a50">
+<script src="https://seo-fixer.writesonic.com/site-audit/fixer-script/index.js" id="wsAiSeoMb" type="application/javascript"><\/script>
+<script id="wsAiSeoInitScript">
+\xA0\xA0wsSEOfixer.configure({
+\xA0\xA0\xA0\xA0hostURL: 'https://seo-fixer.writesonic.com',
+\xA0\xA0\xA0\xA0siteID: '685dc3bfaa6a3035f22cb983'
+\xA0\xA0});
+<\/script>`,description:`Experience Service with 100% organic lotions & lab-certified CBD oil/gummies. Relax, rejuvenate, and trust toxin-free care. Denton’s only salon blending wellness + beauty!`,favicon:`https://framerusercontent.com/assets/m6JhyOa4azRTOEZOYsf2oGkzuA.png`,robots:`max-image-preview:large`,socialImage:`https://framerusercontent.com/assets/amjznIm9h4TrORiur8TKxbVM.png`,title:`Bellevanail.com | Top-Rated Nail Salon in Denton, Texas`}}var n=e((()=>{}));export{n as init_siteMetadata,t as metadata};
+//# sourceMappingURL=siteMetadata.CZpQRxCf.mjs.map
